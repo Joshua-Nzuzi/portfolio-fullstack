abefba0a10f61927eca2f6408fe01addf0243991
Joshua Nzuzi <nzuzijoshua@gmail.com>
2025-11-11 15:52:24 +0000

    Mak Energy website Link update

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -97,7 +97,7 @@ export const projectsData = [
     CompanyInfo: [
       { id: 1, title: 'Name', details: 'Mak Energy' },
       { id: 2, title: 'Services', details: 'Électricité, froid, climatisation, énergie solaire' },
-      { id: 3, title: 'Website', details: 'https://comforting-genie-a4dbdb.netlify.app/' },
+      { id: 3, title: 'Website', details: 'https://mak-energy.com/' },
     ],
     ObjectivesHeading: 'Project Objective',
     ObjectivesDetails:
@@ -118,7 +118,7 @@ export const projectsData = [
     ProjectImages: [
       { id: 1, title: 'Hero Section', img: MakEnergyImage },
     ],
-    LiveSiteLink: 'https://comforting-genie-a4dbdb.netlify.app/',
+    LiveSiteLink: 'https://mak-energy.com/',
   },
 },
 ];
